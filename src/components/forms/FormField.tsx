466fdd72b9dc09eff703374275c3b825a6627cae
Vladislav Nuyanzin <42471298+Faradee@users.noreply.github.com>
2023-12-11 01:41:19 +0300

    ported some formfield styles

--- src/components/forms/FormField.tsx
+++ src/components/forms/FormField.tsx
@@ -1,5 +1,5 @@
 import { IconType } from "react-icons";
-
+import styles from "./formField.module.scss";
 export type FormFieldProps = {
   type: "text" | "password" | "email" | "number" | "textarea" | "boolean";
   icon?: IconType;
@@ -7,10 +7,7 @@ export type FormFieldProps = {
   name: string;
   placeholder?: string;
   rows?: number;
-  onChange?: (
-    e: React.ChangeEvent | React.MouseEvent,
-    setState: React.Dispatch<React.SetStateAction<string>>,
-  ) => void;
+  onChange?: (e: React.ChangeEvent | React.MouseEvent, setState: React.Dispatch<React.SetStateAction<string>>) => void;
   children?: React.ReactNode;
   noMargin?: boolean;
   required?: boolean;
@@ -20,29 +17,13 @@ export type FormFieldProps = {
 
 const FormField = (props: FormFieldProps) => {
   const [state, setState] = props.useState;
-  const {
-    type,
-    icon,
-    placeholder,
-    rows,
-    onChange,
-    children,
-    name,
-    required,
-    noMargin,
-    className,
-    readOnly,
-  } = props;
+  const { type, icon, placeholder, rows, onChange, children, name, required, noMargin, className, readOnly } = props;
   const Icon = icon;
   //Решает нерабочее поведение input number в браузерах Firefox
-  const preventFirefoxNumberInput = (
-    e: React.KeyboardEvent<HTMLInputElement | HTMLTextAreaElement>,
-  ) => {
+  const preventFirefoxNumberInput = (e: React.KeyboardEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     if (
       navigator.userAgent.includes("Firefox") &&
-      !/(^\d*$)|(Backspace|Tab|Delete|ArrowLeft|ArrowRight|Control|Shift|Alt)/.test(
-        e.key,
-      ) &&
+      !/(^\d*$)|(Backspace|Tab|Delete|ArrowLeft|ArrowRight|Control|Shift|Alt)/.test(e.key) &&
       e.key !== "Backspace"
     ) {
       e.preventDefault();
@@ -50,11 +31,7 @@ const FormField = (props: FormFieldProps) => {
   };
   return (
     <label
-      className={
-        className
-          ? ""
-          : "textfield w-full border bg-white focus-within:outline focus-within:outline-2 focus-within:outline-black"
-      }
+      className={styles.label}
       style={
         !className && type === "boolean"
           ? {
@@ -64,8 +41,7 @@ const FormField = (props: FormFieldProps) => {
               padding: "0 2px",
               display: "flex",
               marginLeft: "5px",
-              backgroundColor:
-                state === true ? "rgb(34, 197, 94)" : "rgb(226 232 240)",
+              backgroundColor: state === true ? "rgb(34, 197, 94)" : "rgb(226 232 240)",
               transition: "background-color 0.3s",
               cursor: "pointer",
             }
@@ -78,11 +54,7 @@ const FormField = (props: FormFieldProps) => {
 
       {type === "textarea" ? (
         <textarea
-          className={
-            className
-              ? className
-              : "h-full w-full whitespace-pre-line outline-none"
-          }
+          className={className ? className : styles.textarea}
           value={state as string}
           name={name}
           rows={rows}
@@ -100,42 +72,32 @@ const FormField = (props: FormFieldProps) => {
             type="checkbox"
             name={name}
             defaultChecked={state as boolean}
-            className="hidden"
+            className={styles.hidden}
             onClick={(e) => {
               setState(e);
             }}
           />
-          <div
-            className="ml-0 h-[20px] w-[20px] rounded-full bg-white shadow-lg shadow-black transition-all duration-300"
-            style={state ? { marginLeft: "50%" } : {}}
-          ></div>
+          <div className={styles.checkbox} style={state ? { marginLeft: "50%" } : {}}></div>
         </>
       ) : type === "number" ? (
         <input
-          className={className ? className : "w-full outline-none"}
+          className={className ? className : styles.textfield}
           value={state as string}
           name={name}
           placeholder={placeholder}
           type={type}
           readOnly={readOnly}
           onChange={(e) => {
-            if (
-              /^[1-9][0-9]*$/.test(e.currentTarget.value) ||
-              e.currentTarget.value === ""
-            ) {
-              onChange
-                ? onChange(e, setState)
-                : setState(e.currentTarget.value);
+            if (/^[1-9][0-9]*$/.test(e.currentTarget.value) || e.currentTarget.value === "") {
+              onChange ? onChange(e, setState) : setState(e.currentTarget.value);
             }
           }}
-          onKeyDown={
-            type === "number" ? (e) => preventFirefoxNumberInput(e) : undefined
-          }
+          onKeyDown={type === "number" ? (e) => preventFirefoxNumberInput(e) : undefined}
           required={required}
         />
       ) : (
         <input
-          className={className ? className : "w-full outline-none"}
+          className={className ? className : styles.textfield}
           value={state as string}
           name={name}
           placeholder={placeholder}
