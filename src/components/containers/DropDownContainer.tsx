187cdc3d0a2279d1b5713ab33a22383e496e9877
Vladislav Nuyanzin <42471298+Faradee@users.noreply.github.com>
2023-12-11 00:59:59 +0300

    styled auth modal

--- src/components/containers/DropDownContainer.tsx
+++ src/components/containers/DropDownContainer.tsx
@@ -1,29 +1,7 @@
 import { RefObject, useEffect } from "react";
 
 //хук для обработки клика вне рефа
-export const useClickOutside = (
-  ref: RefObject<HTMLDivElement>,
-  onClickOutside: () => void,
-) => {
-  useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
-      if (ref.current && !ref.current.contains(event.target as Node)) {
-        onClickOutside();
-      }
-    };
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, [ref, onClickOutside]);
-};
-const DropDownContainer = ({
-  children,
-  active,
-}: {
-  children: JSX.Element;
-  active: boolean;
-}) => {
+const DropDownContainer = ({ children, active }: { children: JSX.Element; active: boolean }) => {
   return (
     <div
       className={`container absolute right-0 top-[90%] z-10 mx-auto mt-2 w-[300px] cursor-auto rounded-md border bg-white p-2 shadow-md transition-all duration-100 ${
--- src/components/navbar/AuthModal.tsx
+++ src/components/navbar/AuthModal.tsx
@@ -1,11 +1,13 @@
 "use client";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Image from "next/image";
 import { AiOutlineClose } from "react-icons/ai";
 import { LoaderWrapper } from "../loader/LoaderWrapper";
 import ModalContainer from "../containers/ModalContainer";
 import logo from "/public/vercel.png";
 import AuthForm from "./AuthForm";
+import { useClickOutside } from "../containers/useClickOutside";
+import styles from "./auth.module.scss";
 export type Auth = {
   email: string;
   password: string;
@@ -23,44 +25,31 @@ const AuthModal = ({ setIsAuth }: { setIsAuth: React.Dispatch<React.SetStateActi
   };
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [auth, setAuth] = useState<Auth>(initialAuth);
+  const modalRef = useRef<HTMLDivElement>(null);
   const { isSignup } = auth;
   const handleToggle = () => {
     setAuth({ ...initialAuth, isSignup: !isSignup });
   };
+  useClickOutside(modalRef, () => setIsAuth(false));
   return (
     <ModalContainer>
-      <>
-        {isLoading && <div className="absolute z-10 -mt-4 h-full w-full rounded-3xl bg-white opacity-50"></div>}
-        <div className="w-[175px]">
-          <Image className="" src={logo} alt="Logo" />
-        </div>
-
+      <div ref={modalRef} className={styles.container}>
+        <Image className="" src={logo} width={50} height={50} alt="Logo" />
         {!isLoading && (
-          <div
-            onClick={() => setIsAuth(false)}
-            className="absolute -right-12 top-0 flex h-10 w-10 cursor-pointer items-center justify-center opacity-75 hover:opacity-100"
-          >
+          <div onClick={() => setIsAuth(false)} className={styles.close}>
             <AiOutlineClose size={40} color="white" />
           </div>
         )}
 
-        <h1 className="text-2xl font-semibold">{isSignup ? "Создание аккаунта" : "Вход"}</h1>
+        <h1>{isSignup ? "Создание аккаунта" : "Вход"}</h1>
         <LoaderWrapper loadingState={[isLoading, setIsLoading]}>
           <AuthForm setIsAuth={setIsAuth} setAuth={setAuth} auth={auth} />
         </LoaderWrapper>
-        <div className="flex w-full items-center justify-center before:h-[1px] before:flex-grow before:bg-slate-300 before:content-[''] after:h-[1px] after:flex-grow after:bg-slate-300 after:content-['']">
-          <span className="mx-2">Или</span>
-        </div>
-        <div className="text-center">
+        <div className={styles.switch}>
           {isSignup ? "Есть аккаунт? " : "Не зарегистрированы? "}
-          <span
-            onClick={handleToggle}
-            className="cursor-pointer text-center font-semibold text-blue-600 hover:underline"
-          >
-            {isSignup ? "Войдите" : "Создайте аккаунт"}
-          </span>
+          <span onClick={handleToggle}>{isSignup ? "Войдите" : "Создайте аккаунт"}</span>
         </div>
-      </>
+      </div>
     </ModalContainer>
   );
 };
