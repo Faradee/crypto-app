c0c32b925ccb5aca359ee625058e1dc148783fb7
Vladislav Nuyanzin <42471298+Faradee@users.noreply.github.com>
2023-12-26 02:24:02 +0300

    fixed textfield styles

--- src/components/priceTable/Searchbar.tsx
+++ src/components/priceTable/Searchbar.tsx
@@ -1,13 +1,20 @@
 "use client";
 import { useState } from "react";
+import { IoMdClose } from "react-icons/io";
 import styles from "./searchbar.module.scss";
 import FormField from "../forms/FormField";
+
 const Searchbar = () => {
   const [query, setQuery] = useState<string>("");
-  console.log(query);
   return (
     <div className={styles.container}>
-      <FormField type="text" name="query" useState={[query, setQuery]} />
+      <FormField type="text" name="query" useState={[query, setQuery]}>
+        {query && (
+          <div className={styles.close} onClick={() => setQuery("")}>
+            <IoMdClose size={30} />
+          </div>
+        )}
+      </FormField>
       <button className={styles.button}>Поиск</button>
     </div>
   );
