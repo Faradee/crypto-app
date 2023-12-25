d9b631aa25b9ae653e2e311479782fc0e2473bc8
Vladislav Nuyanzin <42471298+Faradee@users.noreply.github.com>
2023-12-25 03:29:15 +0300

    changed skeleton styles

--- src/components/dashboard/FieldSkeleton.tsx
+++ src/components/dashboard/FieldSkeleton.tsx
@@ -1,12 +1,14 @@
 import Skeleton from "react-loading-skeleton";
 import Slate from "../containers/Slate";
 import styles from "./graphs.module.scss";
-
+import "react-loading-skeleton/dist/skeleton.css";
 const FieldSkeleton = () => {
   return (
     <Slate>
-      <Skeleton className={styles.skeleton} width={"100%"} />
-      <Skeleton className={styles.skeleton} width={"100%"} />
+      <div className={styles.card}>
+        <Skeleton className={styles.skeleton} width={"100%"} />
+        <Skeleton className={styles.skeleton} width={"100%"} />
+      </div>
     </Slate>
   );
 };
