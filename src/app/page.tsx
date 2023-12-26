293034cf2818c12704644a2b3b680f6697b5e5db
Vladislav Nuyanzin <42471298+Faradee@users.noreply.github.com>
2023-12-26 05:05:12 +0300

    removed todos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import PriceTable from "@/components/priceTable/PriceTable";
 import { fetchAssets } from "@/actions/assetActions";
 import { CryptoData, searchAsset } from "@/actions/assetActions";
-//TODO ADD SEARCH
 export default async function Home({ searchParams }: { searchParams: { search: string } }) {
   const data = searchParams.search ? await searchAsset(searchParams.search) : await fetchAssets();
   return <PriceTable data={data} />;
